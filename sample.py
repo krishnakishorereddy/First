aaf2e370930f50b24ae2cfc4a0f9c4c1af012ebf
root <root@localhost.localdomain>
2015-05-06 16:06:30 -0400

    sample.py

---
+++ sample.py
@@ -0,0 +1,10 @@
+
+#!usr/bin/env python
+a=input("Please enter a value")
+b=input("please enter a value")
+print "sum of these numbers",a+b
+print "sub of these numbers",a-b
+print "mutiply of these numbers",a*b
+print "div of these numbers",a/b
+print "power of these numbers",a**b
+
